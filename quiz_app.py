3b5c1296ddec683fcd28c5bae91040fd4156567a
franco <franco.aredes@mi.unc.edu.ar>
2025-07-27 02:08:16 -0300

    implementacion parcial del app. Sigue rota

--- quiz_app.py
+++ quiz_app.py
@@ -2,59 +2,91 @@ import urwid
 import logic
 import database
 
-def show_main_menu():
-    """crea y muestra el menu principal del juego"""
-    # un creador de texto simnple
-    text_header = urwid.Text("--Creador de la Trivia--", align ='center')
-    # crea un widget de boton
-    # urwid.AttrMap es para aplicar estilos
-    # ur2.Button('texto del boton')
-    # urwid.WidgetMap convierte el boton en un widget mapable para urwifd
-    # urwid.on_press es el evento que se dispara cuando se aprieta
-    def exit_program(button):
+class Trivia:
+    def __init__(self):
+        self.current_view = None
+        self.main_loop = None
+        self.show_main_menu()
+        database._create_table()
+
+    def start_game(self, button = None):
+        game_instance = logic.TriviaGame(self.main_loop, self.return_to_menu)
+        self.set_view(game_instance.get_widget(), game_instance.unhandled_input)
+
+    def return_to_menu(self):
+        self.show_main_menu()
+
+    def exit_program(self,button = None):
         raise urwid.ExitMainLoop()
-    play_button = urwid.Button('jugar')
-    urwid.connect_signal(play_button, 'click', lambda button: print("Jugar a la trivia"))
-    add_question_button = urwid.Button('Agregar preg')
-    urwid.connect_signal(add_question_button, 'click', lambda button: print("Agregar preguntas"))
-    exit_button = urwid.Button('Salir')
-    urwid.connect_signal(exit_button, 'click', exit_program)
-    # Agrupa los widgets en una lista
-    body = [
-            urwid.Divider(),
-            urwid.Columns([
-                ('fixed',2,urwid.Text("")), # Espacio a la izquierda
-                urwid.Filler(urwid.Pile([
-                    text_header,
-                    urwid.Divider(),
-                    urwid.Padding(play_button, left = 2, right = 2, width=('relative', 80)),
-                    urwid.Padding(add_question_button, left = 2, right = 2, width=('relative', 80)),
-                    urwid.Padding(exit_button, left= 2, right = 2, width=('relative', 80)),
-                    ]), valign='middle'),
-                ('fixed',2,urwid.Text("")),
-                ]),
-            urwid.Divider(),
+
+    def show_main_menu(self):
+        """crea y muestra el menu principal del juego"""
+        # un creador de texto simnple
+        text_header = urwid.Text("--Creador de la Trivia--", align ='center')
+        # crea un widget de boton
+        # urwid.AttrMap es para aplicar estilos
+        # ur2.Button('texto del boton')
+        # urwid.WidgetMap convierte el boton en un widget mapable para urwifd
+        # urwid.on_press es el evento que se dispara cuando se aprieta
+        play_button = urwid.Button('jugar')
+        urwid.connect_signal(play_button, 'click', self.start_game)
+        add_question_button = urwid.Button('Agregar preg')
+        urwid.connect_signal(add_question_button, 'click',self.add_question_form)
+        exit_button = urwid.Button('Salir')
+        urwid.connect_signal(exit_button, 'click', self.exit_program)
+        # Agrupa los widgets en una lista
+        body = [
+                urwid.Divider(),
+                urwid.Columns([
+                    ('fixed',2,urwid.Text("")), # Espacio a la izquierda
+                    urwid.Filler(urwid.Pile([
+                        text_header,
+                        urwid.Divider(),
+                        urwid.Padding(play_button, left = 2, right = 2, width=('relative', 80)),
+                        urwid.Padding(add_question_button, left = 2, right = 2, width=('relative', 80)),
+                        urwid.Padding(exit_button, left= 2, right = 2, width=('relative', 80)),
+                        ]), valign='middle'),
+                    ('fixed',2,urwid.Text("")),
+                    ]),
+                urwid.Divider(),
+                ]
+        # Crea la vista que mostrará los widgets
+        # urwid.ListBox permite scroll si el contenido es grande 
+        # urwid.Filler centra verticalmente
+        # urwid.Frame permite canecera, pie de pagina y cuerpo
+        list_walker = urwid.SimpleListWalker(body)
+        list_box = urwid.ListBox(list_walker)
+        # Añade padding para que no ocupe todo el ancho
+        padding = urwid.Padding(list_box, align='center', width=('relative', 80))
+        # background = urwid.Overlay(padding, urwid.SolidFill(), 'center', ('relative', 80)
+    def set_view(self, new_widget, unhandled_input_handler=None):
+        if self.main_loop:
+            self.main_loop.widget = new_widget
+            self.main_loop.unhandled_input = unhandled_input_handler if unhandled_input_handler else self.unhandled_input_default
+        else:
+            palete = [
+                ('button', 'black', 'dark green'),
+                ('focus button', 'white', 'dark blue'),
+                ('header', 'white', 'dark red', 'bold'),
+                ('question', 'light cyan', 'black', 'bold'),
+                ('status', 'yellow', 'black'),
+                ('game_over', 'light_red', 'black', 'bold'),
+                ('status_message', 'light magenta', 'black'),
             ]
-    # Crea la vista que mostrará los widgets
-    # urwid.ListBox permite scroll si el contenido es grande 
-    # urwid.Filler centra verticalmente
-    # urwid.Frame permite canecera, pie de pagina y cuerpo
-    list_walker = urwid.SimpleListWalker(body)
-    list_box = urwid.ListBox(list_walker)
-    # Añade padding para que no ocupe todo el ancho
-    padding = urwid.Padding(list_box, align='center', width=('relative', 80))
-    # background = urwid.Overlay(padding, urwid.SolidFill(), 'center', ('relative', 80)
+            self.main_loop = urwid.MainLoop(new_widget, palette=palete, unhandled_input=unhandled_input_handler if unhandled_input_handler else self.unhandled_input_default)
+            self.main_loop.screen.set_terminal_properties(colors=256)
 
-    # El punto de entrada principal del bucle de Urwid
-    # urwid.MonitoredFocusListWalker es util para que la lista maneje el foco
-    loop = urwid.MainLoop(padding, palette=[
-        ('button', 'black', 'dark green'), # color del texto del boton
-        ('focus button' , 'white', 'dark blue'),
-        ('header', 'white', 'dark red', 'bold'), # estilo para el encabezado
-        ])
+    def unhandled_input_default(self, key):
+	    if key in ('q', 'Q'):
+		    exit_program()
 
-    loop.run()
+    def run(self):
+	    self.main_loop.run()
 
+    def add_question_form(self, button = None):
+        form_instance = logic.AddQuestionForm(self.main_loop, self.return_to_menu)
+        self.set_view(form_instance.get_widget(), form_instance.unhandled_input)
 
 if __name__ == "__main__":
-    show_main_menu()
+    app = Trivia()
+    app.run()
